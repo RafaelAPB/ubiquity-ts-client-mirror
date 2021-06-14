94093e7a75475a10b6e9440fbb0ef5113cb85ce5
Andrew Merrigan <amerrigan@blockdaemon.com>
2021-06-14 12:06:06 +0100

    Adding test for functionality

--- Makefile
+++
@@ -1,9 +0,0 @@
-.PHONY: all generate clean
-
-all: generate
-
-generate:
-	@echo "Generating code..."
-	java -jar openapi-generator-cli-5.2.0.jar generate -v -i spec/openapi.yaml -c open-api-conf.yaml -g typescript-axios -o ./src/client-axios
-	
- 
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    testPathIgnorePatterns: ["/lib/", "/node_modules/"],
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    collectCoverage: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
 };
