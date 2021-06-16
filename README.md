af1f9c7314899135539417dcdf47f4903868c6ac
Andrew Merrigan <amerrigan@blockdaemon.com>
2021-06-16 10:07:32 +0100

    Adding additional docs

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## @
 
-This generator creates TypeScript/JavaScript client that utilizes fetch-api. 
-
 ### Building
 
 To build and compile the typescript sources to javascript use:
@@ -10,23 +8,125 @@ npm install
 npm run build
 ```
 
-
-
-### Publishing
-
-First build the package then run ```npm publish```
-
-### Consuming
-
-navigate to the folder of your consuming project and run one of the following commands.
-
-_published:_
-
-```
-npm install @ --save
+### Dependency
+To pull the client from the gitlab repo you will need to add some configuration to the .npmrc
+```bash
+echo "@ubiquity:registry=https://gitlab.com/api/v4/projects/27274533/packages/npm/">>.npmrc
+echo "//gitlab.com/api/v4/projects/27274533/packages/npm/:_authToken=${AUTH_TOKEN}">>.npmrc
+echo "always-auth=true">>.npmrc
 ```
 
-_unPublished (not recommended):_
+### Usage
 
+Create the client by providing a by providing an auth token
+```typescript
+   const client = new UbiquityClient("Auth Token Here");
 ```
-npm install PATH_TO_GENERATED_PACKAGE --save
+
+All of the API's return a promise of type AxiosResponse<T>
+```typescript  
+  client.blocksApi
+    .getBlock(
+      PROTOCOL.BITCOIN,
+      NETWORKS.MAIN_NET,
+      "685700"
+    )
+    .then((balance: AxiosResponse) => console.log(balance.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+```
+
+Initial request to paged API's should not include a continuation. If no limit is supplied the default of 25 will be applied
+A filter can also be applied to select the returned assets
+```typescript  
+  client.accountsApi
+    .getTxsByAddress(
+      PROTOCOL.ETHEREUM,
+      NETWORKS.MAIN_NET,
+      "0x49bC2A9EE1A08dbCa7dd66629700E68AA8DB09aC"
+    )
+```
+
+To continue through the pages of transactions the continuation from the previous page must be supplied to the next request
+```typescript  
+    .then((txsPage1: AxiosResponse<TxPage>) => {
+      console.log(txsPage1);
+      client.accountsApi
+        .getTxsByAddress(
+          PROTOCOL.ETHEREUM,
+          NETWORKS.MAIN_NET,
+          "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+          "desc",
+          txsPage1.data.continuation
+        )
+        .then((txPage2: AxiosResponse<TxPage>) => console.log(txPage2))
+        .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+    })
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+```
+
+All of the API's return a promise of type AxiosResponse<T>
+```typescript  
+  client.platformsApi
+    .getPlatform(
+      PROTOCOL.POLKADOT,
+      NETWORKS.MAIN_NET 
+    )
+    .then((balance: AxiosResponse) => console.log(balance.data))
+    .catch((r: any) => console.log(r));
+```
+
+```typescript   
+  client.syncApi
+    .currentBlockID(
+      PROTOCOL.ETHEREUM,
+      NETWORKS.MAIN_NET
+    )
+    .then((syncData: AxiosResponse<string>) => console.log(syncData.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+```
+
+```typescript
+  client.syncApi
+    .currentBlockNumber(
+      PROTOCOL.ETHEREUM,
+      NETWORKS.MAIN_NET
+    )
+    .then((syncData: AxiosResponse<number>) => console.log(syncData.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+``` 
+
+```typescript
+  client.transactionsApi
+    .getTxs(
+      PROTOCOL.ETHEREUM,
+      NETWORKS.MAIN_NET
+    )
+    .then((syncData: AxiosResponse<TxPage>) => console.log(syncData.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+```
+
+```typescript
+  client.transactionsApi
+    .getTx(
+      PROTOCOL.ETHEREUM,
+      NETWORKS.MAIN_NET,
+      "0x6821b32162ad40f979ad8e999ffbe358e5df0f54e1894d1b3fc3e01fce6a134b"
+    )
+    .then((syncData: AxiosResponse<Tx>) => console.log(syncData.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+```
+
+The generated clients can be used directly for lower level control or in the case of early stage features
+```typescript
+const configuration = new Configuration({accessToken: "Auth Token Here", basePath: "https://ubiquity.api.blockdaemon.com/v2"})
+ 
+  const blockApi = new BlocksApi(configuration);
+  blockApi
+    .getBlock(
+      PROTOCOL.BITCOIN,
+      NETWORKS.MAIN_NET,
+      "00000000000000000001a031c7ff632e6a8c1d95852468aaa17d8cacde17b6de"
+    )
+    .then((r: AxiosResponse<Block>) => console.log(r.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+```
--- example/src/app.ts
+++ example/src/app.ts
@@ -1,47 +1,48 @@
 import { UbiquityClient, NETWORKS, PROTOCOL } from "../../src/client";
-import { BlocksApi, Block, TxPage, Configuration } from "../../src/generated";
+import { BlocksApi, Block, TxPage, Tx, Configuration } from "../../src/generated";
 import { AxiosResponse } from "axios";
 
 async function app(): Promise<void> {
   // To create a client supply an access token
   // Optionally a different base path can be provided
-  const client = new UbiquityClient(
-    "YhLMUNJef5fgjW5cke9Y",
-    "https://stg.ubiquity.api.blockdaemon.com/v2"
-  ); //"---> Auth Token Here");
+  const client = new UbiquityClient("---> Auth Token Here");
 
   // All of the API's return a promise of type AxiosResponse<T>
   client.blocksApi
     .getBlock(
-      PROTOCOL.ALGORAND,
+      PROTOCOL.BITCOIN,
       NETWORKS.MAIN_NET,
-      "HG2JL36OPPITBA7RNIPW4GUQS74AF3SEBO6DAJSLJC33C34I2DQ42F5MU4"
+      "685700"
     )
-    .then((balance: AxiosResponse) => console.log(balance.data));
+    .then((balance: AxiosResponse) => console.log(balance.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
 
   // Initial request to paged API's should not include a continuation.
   // If no limit is supplied the default of 25 will be applied
   // A filter can also be applied to select the returned assets
   client.accountsApi
     .getTxsByAddress(
-      PROTOCOL.DIEM,
+      PROTOCOL.ETHEREUM,
       NETWORKS.MAIN_NET,
       "0x49bC2A9EE1A08dbCa7dd66629700E68AA8DB09aC"
     )
+
     // To continue through the pages of transactions the continuation
     // from the previous page must be supplied to the next request
     .then((txsPage1: AxiosResponse<TxPage>) => {
       console.log(txsPage1);
       client.accountsApi
         .getTxsByAddress(
-          PROTOCOL.DIEM,
+          PROTOCOL.ETHEREUM,
           NETWORKS.MAIN_NET,
-          "0x49bC2A9EE1A08dbCa7dd66629700E68AA8DB09aC",
+          "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
           "desc",
           txsPage1.data.continuation
         )
-        .then((txPage2: AxiosResponse<TxPage>) => console.log(txPage2));
-    });
+        .then((txPage2: AxiosResponse<TxPage>) => console.log(txPage2))
+        .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+    })
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
 
   // All of the API's return a promise of type AxiosResponse<T>
   client.platformsApi
@@ -49,38 +50,60 @@ async function app(): Promise<void> {
       PROTOCOL.POLKADOT,
       NETWORKS.MAIN_NET 
     )
-    .then((balance: AxiosResponse) => console.log(balance.data));
+    .then((balance: AxiosResponse) => console.log(balance.data))
+    .catch((r: any) => console.log(r));
  
-  // All of the API's return a promise of type AxiosResponse<T>
+
+    
   client.syncApi
     .currentBlockID(
       PROTOCOL.ETHEREUM,
       NETWORKS.MAIN_NET
     )
-    .then((syncData: AxiosResponse) => console.log(syncData.data));
+    .then((syncData: AxiosResponse<string>) => console.log(syncData.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+
 
   client.syncApi
     .currentBlockNumber(
       PROTOCOL.ETHEREUM,
       NETWORKS.MAIN_NET
     )
-    .then((syncData: AxiosResponse) => console.log(syncData.data));
+    .then((syncData: AxiosResponse<number>) => console.log(syncData.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+ 
+
+  client.transactionsApi
+    .getTxs(
+      PROTOCOL.ETHEREUM,
+      NETWORKS.MAIN_NET
+    )
+    .then((syncData: AxiosResponse<TxPage>) => console.log(syncData.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+
+
+  client.transactionsApi
+    .getTx(
+      PROTOCOL.ETHEREUM,
+      NETWORKS.MAIN_NET,
+      "0x6821b32162ad40f979ad8e999ffbe358e5df0f54e1894d1b3fc3e01fce6a134b"
+    )
+    .then((syncData: AxiosResponse<Tx>) => console.log(syncData.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
+
 
   // The generated clients can be used directly for lower level control or in the case of early stage features
-  // const configuration = new Configuration({accessToken: "accessToken", basePath: "https://ubiquity.api.blockdaemon.com/v2"})
-  const configuration = new Configuration({
-    accessToken: "YhLMUNJef5fgjW5cke9Y",
-    basePath: "https://stg.ubiquity.api.blockdaemon.com/v2",
-  });
+  const configuration = new Configuration({accessToken: "Auth Token Here", basePath: "https://ubiquity.api.blockdaemon.com/v2"})
+ 
   const blockApi = new BlocksApi(configuration);
-
   blockApi
     .getBlock(
       PROTOCOL.BITCOIN,
       NETWORKS.MAIN_NET,
       "00000000000000000001a031c7ff632e6a8c1d95852468aaa17d8cacde17b6de"
     )
-    .then((r: AxiosResponse<Block>) => console.log(r.data));
+    .then((r: AxiosResponse<Block>) => console.log(r.data))
+    .catch((e: any) => console.log(`error code::${e.response.status} url::${e.config.url}`));
 }
 
 app();
