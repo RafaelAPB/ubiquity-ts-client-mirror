a54e1e9ed5a11e4f4d96fd6e946754ee39297992
Andrew Merrigan <amerrigan@blockdaemon.com>
2021-06-18 11:00:55 +0100

    Fixing export of clients

---
+++ index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./src/client/client";
+export * from "./src/client/constants";
+
+export * from "./src/generated/api";
+export * from "./src/generated/configuration";
+
+ 
