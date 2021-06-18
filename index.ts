7a3d931b19bd31f4c9d996259c28069c78c8406f
Andrew Merrigan <amerrigan@blockdaemon.com>
2021-06-18 12:01:26 +0100

    Adding in npmignore to correct what is being published

--- index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./src/client/client";
-export * from "./src/client/constants";
-
-export * from "./src/generated/api";
-export * from "./src/generated/configuration";
-
- 
